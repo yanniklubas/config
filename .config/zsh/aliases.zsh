9f10b02ce75512c9e7f273ef41b766792dab3bf6
Yannik Lubas <yannik.lubas@gmail.com>
2024-02-22 11:04:09 +0100

    Add zsh

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,11 @@
+alias vim="nvim"
+alias md="mkdir -p"
+alias rd="rmdir"
+
+alias ta="tmux attach -t"
+alias ts="tmux new-session -s"
+alias tl="tmux list-sessions"
+
+alias cls="clear"
+
+alias gcfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
